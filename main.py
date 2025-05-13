10509f4f0c74cc641e049fb028dfe9d03d8f5754
TexasGullstrom <t.gullstrom@gmail.com>
2025-05-13 12:08:38 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ TestDig = np.load("TestDigits.npy")
 TestLab = np.load("TestLabels.npy")
 
 
-
 if True: #SVD,Test Digits
 #siffran 0
     index = (TrainLab == 0) # find train digits of type 0
@@ -98,9 +97,47 @@ if True: #SVD,Test Digits
     index= (TestLab == 9) 
     testdigits_9= TestDig[:,index[0]]
 
-k=15
-r = np.linalg.norm((np.eye(np.shape(U3)[0]) - U3[:,:k]@np.transpose(U3[:,:k]))@d)
+U=np.concatenate((U0,U1,U2,U3,U4,U5,U6,U7,U8,U9),axis=1)
+k=np.linspace(1,40,40)
+TestSolved=np.zeros((40000,1))
+isequal=np.zeros((0,40))
 
+if False: #Fungerande kod
+    for j in range(0,100):
+        r=10000
+        for i in range(0,10):
+            r_i=np.linalg.norm((np.eye(784) - U[:,784*i:784*(i+1)-1][:,:k]@np.transpose(U[:,784*i:784*(i+1)-1][:,:k]))@TestDig[:,j])
+            if r_i<r:
+                r=r_i
+                Digit=i
+            if i==9:#Return the assumed number
+                TestSolved[j]=Digit 
+
+
+    isequal= np.zeros((100,1))
+    for i in range(0,len(TestSolved)):
+        if TestSolved[i] == TestLab[:,i]:
+            isequal[i]=1
+
+    print(np.sum(isequal))
+
+
+for p in range(0,40):
+    r=np.linalg.norm((np.eye(784) - U0[:,:int(k[p])]@np.transpose(U0[:,:int(k[p])]))@TestDig,axis=0)
+
+    for i in range(0,9):
+        r_i=np.linalg.norm((np.eye(784) - U[:,784*(i+1):784*(i+2)-1][:,:int(k[p])]@np.transpose(U[:,784*(i+1):784*(i+2)-1][:,:int(k[p])]))@TestDig,axis=0)
+        r=np.vstack((r,r_i))
+
+    TestSolved=np.argmin(r,axis=0)
+    isequal_i= np.zeros((40000,1))
+    for i in range(0,len(TestSolved)):
+        if TestSolved[i] == TestLab[:,i]:
+            isequal_i[i]=1
+    isequal[p]=np.sum(isequal_i)/40000
+
+plt.plot(k,isequal)
+plt.show()
 
 if False: #Plot Singlular values
     plt.plot(np.linspace(0,len(S0),len(S0)),S0) #Singular values for 0
@@ -116,7 +153,6 @@ if False: # Visar den perfekta 3:an
     plt.imshow(D3, cmap ="gray") # Den perfekta 3:an
     plt.show()
  
-d = TestDig[:,7] # en testbild
 
 if False:
     D = np.reshape(d, (28, 28)).T # Reshaping a vector to a matrix
@@ -128,12 +164,8 @@ if False:
 
 # Beräkna residual
 
-index= (TestLab==0)
-
 
 # beräkna residual, exempel 
-k=15
-r = np.linalg.norm((np.eye(np.shape(U3)[0]) - U3[:,:k]@np.transpose(U3[:,:k]))@d)
 
 # jämför med alla
 
